253953a6a4e53c4d8165d1714b98f291b7029052
Frederick Ros <frederick.ros@gmail.com>
2013-06-17 17:02:55 +0200

    Ensure tests are passing

--- ui/test/spec/controllers/ChatroomsList.js
+++ ui/test/spec/controllers/ChatroomsList.js
@@ -8,15 +8,53 @@ describe('Controller: ChatroomsListCtrl', function () {
   var ChatroomsListCtrl,
     scope;
 
+  var locationMock = {
+    _path: "",
+    path: function(p) { _path = p; },
+    _getPath: function() { return p; }
+  };
+
+  var pageMock = {
+    _title: "foo",
+    setTitle: function(t) { this._title = t;},
+    _getTitle: function() { return this._title; }
+  };
+
+  var chatroomsMock = {
+    getChatrooms: function(cb){
+      cb([{ name: 'foo', userCount: 2}]);
+    },
+    createChatroom: function(name, cb) {
+
+    }
+  };
+
+
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
     ChatroomsListCtrl = $controller('ChatroomsListCtrl', {
-      $scope: scope
+      $scope: scope,
+      $location: locationMock,
+      Page: pageMock,
+      Chatrooms: chatroomsMock
     });
   }));
 
   it('should attach a list of chatrooms to the scope', function () {
     expect(scope.chatrooms);
+  });
+
+  it('should set an empty title', function() {
+    expect(pageMock._getTitle()).toEqual("");
+  });
+
+  it('should set the list of chatrooms', function() {
+    expect(scope.chatrooms).toBeDefined();
+    expect(scope.chatrooms.length).toEqual(1);
+    var chatroom = scope.chatrooms[0];
+
+    expect(chatroom.name).toEqual('foo');
+    expect(chatroom.userCount).toEqual(2);
   });
 });
